289c9cb4c3edf50de2cf2e8fe41c8174c3bd6b49
yoousuph <yuskay001@gmail.com>
2026-07-12 20:10:03 +0100

    changed description

--- db-sg.tf
+++ db-sg.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "db_sg" {
   name        = var.db_sg_name
-  description = "Identifier for the RDS database instance"
+  description = var.db_sg_name
   vpc_id      = aws_vpc.three_tier_vpc.id
 
   ingress {
